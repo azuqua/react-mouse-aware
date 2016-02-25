98a3e47c0b62b505a1283fb6b15e14835dd0bafd
Nicholas Clawson <nickclaw@gmail.com>
2016-02-25 10:17:10 -0800

    bash scripts -> npm scripts

--- scripts/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -r lib
-mkdir lib
-node_modules/.bin/babel src --out-dir lib
--- scripts/publish.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-./scripts/test.sh
-./scripts/build.sh
-npm version $1
-git push origin master --tags
-npm publish
